c8e18e4b9ddcf0d51bb6edad849e07dbbd40c393
Yoann LE TOUCHE <kurotoshiro@gmail.com>
2011-01-19 21:54:19 +0100

    Week1 - challenge formatting done

--- week1/challenge_formatting.rb
+++ week1/challenge_formatting.rb
@@ -7,4 +7,64 @@
   * Date        : 2011-01-19
   * License     : MIT license
 =end
+
+#
+# Give the size of a cell
+#
+def cell_size(i)
+  i.to_s.size+1
+end
+
+#
+# Create a line of the multiplication table
+#
+def line(number,nb_col,size)
+  line=''
+  nb_col.times do |i|
+    i+=1
+    line+="%#{size}d" % (number*i)
+  end
+  line+"\n"
+end
+
+#
+# Create some decoration the right size
+#
+def decoration(size,char='=')
+  " "+char.to_s*size+"\n"
+end
+
+#
+# Create a centered header (given a line size)
+#
+def header(text,size)
+  text.center(size+1)+"\n"
+end
+
+#
+# Return the multiplication table to integer
+#
+def multiplication_table(integer, heading = '', decorate = false)
+  table=''
+  cell_size=cell_size(integer**2)
+  decoration=decoration(cell_size*integer)
+
+
+  table+=header(heading,cell_size*integer+1) if not heading.empty?
+  table+=decoration if decorate
  
+  integer.times do |i|
+    i+=1
+    table+=line(i,integer,cell_size)
+  end
+  
+  table+=decoration if decorate
+  
+  table
+end
+
+puts multiplication_table(9)
+puts
+puts multiplication_table(9,heading='Times Table to 9')
+puts
+puts multiplication_table(9,heading='Times Table to 9',true)
